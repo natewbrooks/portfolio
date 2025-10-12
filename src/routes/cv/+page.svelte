7c348d0244b15209d22502249bdf21ac9740dacd
Nate <natewbrooks@gmail.com>
2025-10-12 14:22:13 -0400

    Add padding to bottom of cv

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -2,7 +2,7 @@
 
 </script>
 
-<div class="pt-8 px-4 h-full flex flex-col space-y-2 min-h-screen">
+<div class="py-8 px-4 h-full flex flex-col space-y-2 min-h-screen">
     <div class="w-full flex items-center justify-between">
         <h1 class="">Nathaniel Brooks Resume</h1>
         <a href="/" class="text-pink">return</a>
