79dbb64c66d072784a501ca13b943bdc9be0ff16
n8 ô¿ô <natewbrooks@gmail.com>
2025-10-12 17:39:40 -0400

    Update cv padding

--- src/routes/cv/+page.svelte
+++ src/routes/cv/+page.svelte
@@ -2,10 +2,10 @@
 
 </script>
 
-<div class="relative py-8 px-4 h-full flex flex-col space-y-2 min-h-screen">
+<div class="relative py-4 px-4 h-full flex flex-col space-y-2 min-h-screen">
     <div class="sticky top-0 w-full flex items-center justify-between bg-darkest py-2">
         <h1 class="">Nathaniel Brooks Resume</h1>
-        <a href="/" class="text-pink">return home</a>
+        <a href="/" class="text-pink">return</a>
     </div>
     <embed src={"NathanielBrooksResume.pdf"} class="w-full h-[100vh]">
 </div>
