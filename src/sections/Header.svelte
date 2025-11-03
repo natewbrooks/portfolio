3378fea1b0aec5bb0406e2f02c788aea725f2af9
Nate <natewbrooks@gmail.com>
2025-11-03 09:26:11 -0500

    Increase bio font and update resume

--- src/sections/Header.svelte
+++ src/sections/Header.svelte
@@ -70,7 +70,7 @@
         <StatusMarquee/>
         <div class="px-4 lg:px-0 border-light border-t-1 pt-6 ">
             <!-- {#if revealAbout} -->
-                <p transition:slide={{axis: "y"}} class="  text-xs pb-4 -z-10">Hello! I’m 21 years old and am very passoniate about programming and have been since I was around 10 years old developing Minecraft plugins. I love learning and building with new tech stacks and can only sleep comfortably when my current problem is solved. <br/><br/>Every project teaches me something new. I have a passion for UI/UX design and architecting projects that will help others.  My goal is simple: I want to know <span class="text-pink">I can build anything</span> with absolute confidence.</p>
+                <p transition:slide={{axis: "y"}} class="  text-sm pb-4 -z-10">Hello! I’m 21 years old and am very passoniate about programming and have been since I was around 10 years old developing Minecraft plugins. I love learning and building with new tech stacks and can only sleep comfortably when my current problem is solved. <br/><br/>Every project teaches me something new. I have a passion for UI/UX design and architecting projects that will help others.  My goal is simple: I want to know <span class="text-pink">I can build anything</span> with absolute confidence.</p>
             <!-- {/if} -->
         </div>
     </header>
