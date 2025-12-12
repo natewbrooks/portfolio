e2ee3bbcc0194a6d3bd29d162b71a983e4c8f467
Nate <natewbrooks@gmail.com>
2025-12-12 18:29:12 -0500

    Fix marquee

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,27 @@
+import animate from 'tailwindcss-animate';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{html,js,svelte,ts}',
     './node_modules/@selemondev/svelte-marquee/dist/*.svelte'
   ],
+  theme: {
+		extend: {
+			keyframes: {
+				'marquee-left': {
+					from: { transform: 'translateX(0)' },
+					to: { transform: 'translateX(calc(-100% - var(--gap)))' }
+				},
+				'marquee-up': {
+					from: { transform: 'translateY(0)' },
+					to: { transform: 'translateY(calc(-100% - var(--gap)))' }
+				}
+			},
+			animation: {
+				'marquee-left': 'marquee-left var(--duration, 40s) linear infinite',
+				'marquee-up': 'marquee-up var(--duration, 40s) linear infinite'
+			}
+		}
+	},
+	plugins: [animate]
 }
