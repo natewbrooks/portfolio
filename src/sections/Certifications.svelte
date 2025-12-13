18aa12a182cb72b544b59c4826b3f4212fdd71d6
Nate <natewbrooks@gmail.com>
2025-12-12 22:54:07 -0500

    Add border to certifications

--- src/sections/Certifications.svelte
+++ src/sections/Certifications.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
 	import { certifications } from "$lib/types/certs";
+
+  const COLOR = {
+    orange: { text: "text-orange", border: "border-orange" },
+    blue: { text: "text-blue", border: "border-blue" },
+    purple: { text: "text-purple", border: "border-purple" },
+    pink: { text: "text-pink", border: "border-pink" },
+  } as const;
 </script>
 
 
@@ -9,8 +16,9 @@
   <br/>
   <div class="max-h-[220px] overflow-y-auto ">
     <ul class="list-none ml-4 flex flex-col space-y-1">
-      {#each certifications as cert}
-        <li class="">
+      {#each certifications as cert, i}
+      {@const c = COLOR[(Object.keys(COLOR)[i % Object.keys(COLOR).length]) as keyof typeof COLOR]}
+        <li class={`border-l-2 ${c.border} pl-4`}>
             <p class="inline">{cert.name}</p>
             <span class={"text-white/50 opacity-50"}>{cert.date}</span>
         </li>
