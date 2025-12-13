ba144f907af9ae596ee16bbcd0afb818c4ce4047
Nate <natewbrooks@gmail.com>
2025-12-12 22:51:33 -0500

    Update certification date color

--- src/sections/Certifications.svelte
+++ src/sections/Certifications.svelte
@@ -12,7 +12,7 @@
       {#each certifications as cert}
         <li class="">
             <p class="inline">{cert.name}</p>
-            <span class={"text-white/60"}>{cert.date}</span>
+            <span class={"text-white/50 opacity-50"}>{cert.date}</span>
         </li>
       {/each}
     </ul>
