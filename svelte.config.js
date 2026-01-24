24344dcd52f04a065bc509d6660d3947df63bb8e
Nate <natewbrooks@gmail.com>
2026-01-24 09:54:10 -0500

    Change for railway not netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,13 @@
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-	kit: {
-		// default options are shown
-		adapter: adapter({
-			// if true, will create a Netlify Edge Function rather
-			// than using standard Node-based functions
-			edge: false,
-
-			// if true, will split your app into multiple functions
-			// instead of creating a single one for the entire app.
-			// if `edge` is true, this option cannot be used
-			split: false
-		})
-	}
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter({ out: 'build' })
+  }
 };
 
 export default config;
