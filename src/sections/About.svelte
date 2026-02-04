ba9052c8102a3f9ded0f328d5ea3caafb024ca43
Nate <natewbrooks@gmail.com>
2026-02-04 12:06:44 -0500

    Update bio

--- src/sections/About.svelte
+++ src/sections/About.svelte
@@ -1,9 +1,9 @@
-<div class="text-sm">
+<div class="text-md">
     <p class="pb-4 -z-10">
-        Hello! I’m 21 years old and have been <span class="text-green font-bold">passionate about programming</span> since I was around 10, when I began building <span>Minecraft plugins</span>.
+        <!-- Hello! I’m 21 years old and have been <span class="text-green font-bold">passionate about programming</span> since I was around 10, when I began building <span>Minecraft plugins</span>. -->
+        <!-- <br /><br /> -->
+        Hello! I'm 21 years old and have been passionate about programming from a young age. I approach every project as an opportunity to learn something new and hold myself to a high standard. I’m especially drawn to designing elegant <span class="text-purple font-bold">UI/UX</span> and <span class="text-blue font-bold">system architecture</span> problems that scale effectively.
         <br /><br />
-        Every project teaches me something new. I enjoy learning new technologies and working across modern stacks. I’m especially drawn to <span class="text-purple font-bold">UI/UX design</span> and <span class="text-blue font-bold">system architecture</span> that solve real problems and scale effectively.
-        <br /><br />
-        <span class="text-pink font-bold">My ultimate goal is to develop software that has a lasting, positive impact.</span> 
+        <span class="text-pink font-bold">My long-term goal is to build software that has a lasting, positive impact on real people.</span> 
     </p>
 </div>
